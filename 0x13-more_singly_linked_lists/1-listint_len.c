48481c54a547ce63b4b5c7fbf21cd3667d78c114
mohammed ahmed <93189232+mohamed12344556@users.noreply.github.com>
2023-05-03 02:56:27 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -15,15 +15,16 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t num = 0;
 
 	while (h)
 	{
-		nodes++;
+		num++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (num);
 }
+
 
 
