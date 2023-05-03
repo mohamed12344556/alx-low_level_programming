f2354f92ff13cf24616120a9f48b5062a6bc6fd2
mohammed ahmed <93189232+mohamed12344556@users.noreply.github.com>
2023-05-03 03:14:26 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,18 @@
+/*
+ * File: 7-get_nodeint.c
+ * Auth: Mohamed Ahmed
+ */
+
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * Get_nodeint_at_index - returns the nth node of a listint_t linked list.
  * @head: pointer to the list.
  * @index: nth node.
- * Return: nth node, if the node doesn't exist, returns NULL.
+ * Return: Nth node, if the node does not exist, returns NULL.
  **/
+
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *node = head;
