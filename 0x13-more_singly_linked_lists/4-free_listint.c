32e78e439998b162b430e69b8641305be9064a32
mohammed ahmed <93189232+mohamed12344556@users.noreply.github.com>
2023-05-03 03:40:04 +0300

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -13,19 +13,12 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *actual_node;
-	listint_t *next_node;
+	listint_t *temp;
 
-	if (head)
+	while (head)
 	{
-		actual_node = head;
-		next_node = head->next;
-		while (next_node)
-		{
-			free(actual_node);
-			actual_node = next_node;
-			next_node = next_node->next;
-		}
-		free(actual_node);
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
