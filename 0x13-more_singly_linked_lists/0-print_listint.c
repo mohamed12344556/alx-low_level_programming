c0300b83749d2a2bb3f95a0c893f6a3b03ca48fc
mohammed ahmed <93189232+mohamed12344556@users.noreply.github.com>
2023-05-03 04:28:20 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * Print_listint - prints all of the factors of a listint_t listing.
- * @h: pointer to the list.
- * Return: number of nodes.
- **/
-
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
@@ -19,6 +19,3 @@ size_t print_listint(const listint_t *h)
 
 	return (num);
 }
-
-
-
