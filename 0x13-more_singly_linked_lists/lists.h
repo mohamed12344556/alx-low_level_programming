53bf654d29189fa3efdb760ae3bc4094f8ecaa99
mohammed ahmed <93189232+mohamed12344556@users.noreply.github.com>
2023-05-03 03:35:37 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,9 +1,3 @@
-#ifndef LISTS
-#define LISTS
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
 /*
  * File: lists.h
  * Auth: Mohamed Ahmed
@@ -11,6 +5,12 @@
  *       and brands written in the 0x12-more_singly_linked_lists listing.
  */
 
+
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -20,32 +20,34 @@
  * Description: singly linked list node structure
  * for alx project
  */
-
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
 
-listint_t *find_listint_loop(listint_t *head);
-size_t free_listint_safe(listint_t **h);
-size_t print_listint_safe(const listint_t *head);
-
-listint_t *reverse_listint(listint_t **head);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int pop_listint(listint_t **head);
+///
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-
-int sum_listint(listint_t *head);
-int pop_listint(listint_t **head);
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-
 void free_listint2(listint_t **head);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
+///
+int sum_listint(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+size_t print_listint_safe(const listint_t *head);
+///
 
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *reverse_listint(listint_t **head);
+void free_listint(listint_t *head);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+///
+
+listint_t *find_listint_loop(listint_t *head);
 size_t listint_len(const listint_t *h);
 size_t print_listint(const listint_t *h);
 
 
 #endif
+
